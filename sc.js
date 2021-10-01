5c1d728e70d67ee771314ca79e260a0ffba1d28c
koro <me@krbk.dev>
2021-10-01 22:02:33 +0200

    :sparkles: Introduce code example

--- sc.js
+++ sc.js
@@ -34,4 +34,70 @@
 
     write();
 
+})();
+
+(()=>{
+    const codeel = document.getElementById("writecode");
+    let texts = [
+        "const Client = require('ubereats.js');",
+        "const uber = new Client();",
+        "",
+        "// Look for a tacos store",
+        "uber.searchStores(\"tacos\").then((stores) => {",
+        "   // Check for count",
+        "   if (response.length == 0) return console.log(\"No tacos store found!\");",
+        "",
+        "   // Get store 1 menu",
+        "   uber.getStoreMenu(stores[0].uuid).then((menu) => {",
+        "       console.log(menu);",
+        "   }).catch(err => console.log);",
+        "}).catch(err => console.log);"
+    ];
+    let current = 0;
+    let _current = 0;
+    let _i = setInterval(()=>{
+        if(isScrolledIntoView(codeel)){
+            clearInterval(_i);
+            let i = setInterval(()=>{
+                const text = texts[current];
+                let _text = [...text];
+                let value = codeel.innerText;
+
+                if (!_text[_current]) {
+                    codeel.innerText = value+"\n";
+                    current++;
+                    _current = 0;
+                    if (current == texts.length) {
+                        return clearInterval(i);
+                    }
+                } else {
+                    codeel.innerText = value+_text[_current];
+                    value = codeel.innerText;
+                    _current++;
+                    if (_current == text.length) {
+                        current++;
+                        _current = 0;
+                        codeel.innerText = value+"\n";
+                        if (current == texts.length) {
+                            return clearInterval(i);
+                        }
+                    }
+                }
+            }, 60)
+        }
+    }, 100)
 })()
+
+function isScrolledIntoView(el) {
+    const rect = el.getBoundingClientRect();
+    const elemTop = rect.top;
+    const elemBottom = rect.bottom;
+
+    // Only completely visible elements return true:
+    const isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    // Partially visible elements return true:
+    //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    return isVisible;
+}
+
+AOS.init();
