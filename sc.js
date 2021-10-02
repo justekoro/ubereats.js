7e805ab4dd2af36a4f21424db3aa0ec754977f14
koro <me@krbk.dev>
2021-10-02 22:08:23 +0200

    small changes

--- sc.js
+++ sc.js
@@ -92,11 +92,7 @@ function isScrolledIntoView(el) {
     const rect = el.getBoundingClientRect();
     const elemTop = rect.top;
     const elemBottom = rect.bottom;
-
-    // Only completely visible elements return true:
     const isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-    // Partially visible elements return true:
-    //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
     return isVisible;
 }
 
